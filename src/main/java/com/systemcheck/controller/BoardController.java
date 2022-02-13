0f24c1ef3d6057f4a0a71f737bd2a32713fc0a84
mhw <onohiyn@hotmail.com>
2022-02-13 22:25:51 +0900

    file download added - (not working in iphone)

--- src/main/java/com/systemcheck/controller/BoardController.java
+++ src/main/java/com/systemcheck/controller/BoardController.java
@@ -12,14 +12,19 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.UUID;
 
 
@@ -82,8 +87,23 @@ public class BoardController {
     }
 
 
+    @CrossOrigin(value = {"*"})
+    @RequestMapping(value = "/filedownload", method= {RequestMethod.POST, RequestMethod.GET})
+    public ResponseEntity<?> fileDownload (@RequestBody FileEntity param) throws Exception {
+        String fileFullPath = fileLocation + param.getUuid();
+        try{
+            Path filePath = Paths.get(fileFullPath);
+            Resource resource = new InputStreamResource(Files.newInputStream(filePath));
 
-
-
+            File file = new File(fileFullPath);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentDisposition(ContentDisposition.builder("attachment").filename(file.getName()).build());
+            headers.setAccessControlExposeHeaders(Arrays.asList("Content-Disposition"));
+            headers.setAccessControlAllowHeaders(Arrays.asList("Content-Disposition"));
+            return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+        } catch(Exception e){
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
 
 }
