8bc8bf117aff7205d923b9551e25405fb3d19c5a
moonhw <onohiyn@hotmail.com>
2022-07-25 21:51:46 +0900

    외부 API 테스트용 API 추가 - Json Null 제거

--- src/main/java/com/systemcheck/entity/CoffeeOrder.java
+++ src/main/java/com/systemcheck/entity/CoffeeOrder.java
@@ -1,10 +1,12 @@
 package com.systemcheck.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CoffeeOrder {
 
     private String customerId;
